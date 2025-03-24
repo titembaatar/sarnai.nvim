48a968039e55edad7d2f02926333f48e6316e432
titembaatar <etienne.guillemin@protonmail.com>
2025-03-24 22:51:13 +0100

    init

--- lua/sarnai/init.lua
+++ lua/sarnai/init.lua
@@ -1,19 +1,38 @@
+local config = require("sarnai.config")
+local colors = require("sarnai.colors")
+local util = require("sarnai.util")
+
 local M = {}
 
 function M.setup(opts)
   -- Store the options for later use
-  M.options = vim.tbl_deep_extend("force", M.defaults, opts or {})
+  M.options = vim.tbl_deep_extend("force", config.defaults, opts or {})
 end
 
 function M.load(opts)
   -- Load the colorscheme with given options
-  opts = vim.tbl_deep_extend("force", M.options or M.defaults, opts or {})
-  print("Sarnai theme loaded with style: " .. opts.style)
-end
+  opts = vim.tbl_deep_extend("force", M.options or config.defaults, opts or {})
 
--- Default configuration
-M.defaults = {
-  style = "khavar"
-}
+  -- Get the color palette
+  local palette = colors.get_colors(opts.style)
+
+  -- Clear any existing highlights if not the default colorscheme
+  if vim.g.colors_name then
+    vim.cmd("hi clear")
+  end
+
+  -- Set terminal colors if enabled
+  vim.o.termguicolors = true
+  if opts.terminal_colors ~= false then
+    util.set_terminal_colors(palette)
+  end
+
+  vim.g.colors_name = "sarnai-" .. opts.style
+
+  -- For now, just print that we loaded (we'll add highlight setup later)
+  print("Sarnai theme loaded with style: " .. opts.style)
+
+  return palette
+end
 
 return M
