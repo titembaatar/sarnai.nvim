eed9cc9709b3174ddec64c227efa6730cbf9d5d3
titembaatar <etienne.guillemin@protonmail.com>
2026-01-25 19:04:17 +0100

    cleanup

--- lua/sarnai/highlights/plugins/trouble.lua
+++ lua/sarnai/highlights/plugins/trouble.lua
@@ -4,16 +4,16 @@ local M = {}
 ---@param opts SarnaiConfig
 ---@return Highlights
 function M.get(colors, opts)
-  local p = colors.palette
-  local ui = colors.ui
+	local p = colors.palette
+	local ui = colors.ui
 
-  local styles = opts.styles or {}
+	local styles = opts.styles or {}
 
-  return {
-    TroubleNormal = { fg = ui.fg, bg = ui.bg_float },
-    TroubleText = { fg = p.subtle },
-    TroubleCount = { fg = p.yargui, bg = p.overlay, bold = styles.bold },
-  }
+	return {
+		TroubleNormal = { fg = ui.fg, bg = ui.bg_float },
+		TroubleText   = { fg = p.subtle },
+		TroubleCount  = { fg = ui.accent, bg = p.overlay, bold = styles.bold },
+	}
 end
 
 return M
