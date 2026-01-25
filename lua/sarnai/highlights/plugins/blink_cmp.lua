0c58768a2b08303c8a3626007a70cc7ea4eced02
titembaatar <etienne.guillemin@protonmail.com>
2026-01-25 19:01:45 +0100

    cleanup

--- lua/sarnai/highlights/plugins/blink_cmp.lua
+++ lua/sarnai/highlights/plugins/blink_cmp.lua
@@ -1,17 +1,16 @@
 local M = {}
 
 ---@param colors ColorPalette
----@param opts SarnaiConfig
 ---@return Highlights
-function M.get(colors, opts)
+function M.get(colors)
   local p = colors.palette
   local ui = colors.ui
-  local t = colors.terminal
 
   return {
+	BlinkCmpCursorLineMenuHack  = { bg = p.surface },
     BlinkCmpDoc                 = { fg = ui.fg, bg = ui.bg_float },
     BlinkCmpDocBorder           = { fg = ui.border, bg = ui.bg_float },
-    BlinkCmpGhostText           = { fg = t.bright_black },
+    BlinkCmpGhostText           = { fg = p.surface },
     BlinkCmpKindCodeium         = { fg = p.mus, bg = colors.none },
     BlinkCmpKindCopilot         = { fg = p.mus, bg = colors.none },
     BlinkCmpKindDefault         = { fg = p.subtle, bg = colors.none },
@@ -21,7 +20,7 @@ function M.get(colors, opts)
     BlinkCmpLabelDeprecated     = { fg = p.muted, bg = colors.none, strikethrough = true },
     BlinkCmpLabelMatch          = { fg = ui.accent, bg = colors.none },
     BlinkCmpMenu                = { fg = ui.fg, bg = ui.bg_float },
-    BlinkCmpMenuBorder          = { fg = ui.border, bg = ui.bg_float },
+	BlinkCmpMenuBorder          = { fg = ui.border, bg = ui.bg_float },
     BlinkCmpSignatureHelp       = { fg = ui.fg, bg = ui.bg_float },
     BlinkCmpSignatureHelpBorder = { fg = ui.border, bg = ui.bg_float },
   }
