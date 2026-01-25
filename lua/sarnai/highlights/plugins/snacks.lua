79e3b4ccbf3829cd1d1300d2d5cf0b95ae3a3197
titembaatar <etienne.guillemin@protonmail.com>
2026-01-25 18:27:25 +0100

    cleanup

--- lua/sarnai/highlights/plugins/snacks.lua
+++ lua/sarnai/highlights/plugins/snacks.lua
@@ -1,47 +1,49 @@
-local util = require("sarnai.util")
-
 local M = {}
 
 ---@param colors ColorPalette
 ---@param opts SarnaiConfig
 ---@return Highlights
 function M.get(colors, opts)
-  local p = colors.palette
-  local sem = colors.semantic
-  local ui = colors.ui
+	local p = colors.palette
+	local sem = colors.semantic
+	local ui = colors.ui
 
-  return {
-    -- Dashboard
-    SnacksDashboardDesc       = { fg = ui.fg },
-    SnacksDashboardFooter     = { fg = p.subtle },
-    SnacksDashboardHeader     = { fg = ui.accent },
-    SnacksDashboardIcon       = { fg = ui.accent },
-    SnacksDashboardKey        = { fg = p.nuur },
-    SnacksDashboardSpecial    = { fg = p.chatsalgan },
-    SnacksDashboardDir        = { fg = p.mus },
+	return {
+		-- Dashboard
+		SnacksDashboardDir      = { fg = p.mus },
+		SnacksDashboardKey      = { fg = p.nuur },
+		SnacksDashboardDesc     = { fg = ui.fg },
+		SnacksDashboardIcon     = { fg = ui.accent },
+		SnacksDashboardFooter   = { fg = p.subtle },
+		SnacksDashboardHeader   = { fg = ui.accent },
+		SnacksDashboardSpecial  = { fg = p.chatsalgan },
+		SnacksDashboardTitle    = { fg = ui.accent },
+		SnacksDashboardFile     = { fg = ui.fg },
+		SnacksDashboardNormal   = { fg = ui.fg },
+		SnacksDashboardTerminal = { fg = ui.bg },
 
-    -- Notifier
-    SnacksNotifierDebug       = { fg = ui.fg, bg = opts.transparent and colors.none or ui.bg },
-    SnacksNotifierBorderDebug = { fg = p.subtle, bg = opts.transparent and colors.none or ui.bg },
-    SnacksNotifierIconDebug   = { fg = sem.hint },
-    SnacksNotifierTitleDebug  = { fg = sem.hint },
-    SnacksNotifierError       = { fg = ui.fg, bg = opts.transparent and colors.none or ui.bg },
-    SnacksNotifierBorderError = { fg = sem.error, bg = opts.transparent and colors.none or ui.bg },
-    SnacksNotifierIconError   = { fg = sem.error },
-    SnacksNotifierTitleError  = { fg = sem.error },
-    SnacksNotifierInfo        = { fg = ui.fg, bg = opts.transparent and colors.none or ui.bg },
-    SnacksNotifierBorderInfo  = { fg = sem.info, bg = opts.transparent and colors.none or ui.bg },
-    SnacksNotifierIconInfo    = { fg = sem.info },
-    SnacksNotifierTitleInfo   = { fg = sem.info },
-    SnacksNotifierTrace       = { fg = ui.fg, bg = opts.transparent and colors.none or ui.bg },
-    SnacksNotifierBorderTrace = { fg = sem.ok, bg = opts.transparent and colors.none or ui.bg },
-    SnacksNotifierIconTrace   = { fg = sem.ok },
-    SnacksNotifierTitleTrace  = { fg = sem.ok },
-    SnacksNotifierWarn        = { fg = ui.fg, bg = opts.transparent and colors.none or ui.bg },
-    SnacksNotifierBorderWarn  = { fg = sem.warn, bg = opts.transparent and colors.none or ui.bg },
-    SnacksNotifierIconWarn    = { fg = sem.warn },
-    SnacksNotifierTitleWarn   = { fg = sem.warn },
-  }
+		-- Notifier
+		SnacksNotifierInfo        = { fg = ui.fg, bg = opts.transparent and colors.none or ui.bg },
+		SnacksNotifierIconInfo    = { fg = sem.info },
+		SnacksNotifierTitleInfo   = { fg = sem.info },
+		SnacksNotifierBorderInfo  = { fg = sem.info, bg = opts.transparent and colors.none or ui.bg },
+		SnacksNotifierWarn        = { fg = ui.fg, bg = opts.transparent and colors.none or ui.bg },
+		SnacksNotifierIconWarn    = { fg = sem.warn },
+		SnacksNotifierTitleWarn   = { fg = sem.warn },
+		SnacksNotifierBorderWarn  = { fg = sem.warn, bg = opts.transparent and colors.none or ui.bg },
+		SnacksNotifierDebug       = { fg = ui.fg, bg = opts.transparent and colors.none or ui.bg },
+		SnacksNotifierIconDebug   = { fg = sem.hint },
+		SnacksNotifierTitleDebug  = { fg = sem.hint },
+		SnacksNotifierBorderDebug = { fg = p.subtle, bg = opts.transparent and colors.none or ui.bg },
+		SnacksNotifierError       = { fg = ui.fg, bg = opts.transparent and colors.none or ui.bg },
+		SnacksNotifierIconError   = { fg = sem.error },
+		SnacksNotifierTitleError  = { fg = sem.error },
+		SnacksNotifierBorderError = { fg = sem.error, bg = opts.transparent and colors.none or ui.bg },
+		SnacksNotifierTrace       = { fg = ui.fg, bg = opts.transparent and colors.none or ui.bg },
+		SnacksNotifierIconTrace   = { fg = sem.ok },
+		SnacksNotifierTitleTrace  = { fg = sem.ok },
+		SnacksNotifierBorderTrace = { fg = sem.ok, bg = opts.transparent and colors.none or ui.bg },
+	}
 end
 
 return M
