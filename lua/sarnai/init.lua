01d5a8691ab0918dc3dbc50eea9f0542dcc069ab
titembaatar <etienne.guillemin@protonmail.com>
2026-01-28 00:14:46 +0100

    removed cache temporarly and replace Groups.get() and Groups.set() to a unique Groups.setup()

--- lua/sarnai/init.lua
+++ lua/sarnai/init.lua
@@ -1,6 +1,5 @@
 local Config = require("sarnai.config")
 local Colors = require("sarnai.colors")
-local Cache = require("sarnai.cache")
 local Term = require("sarnai.terminal")
 local Groups = require("sarnai.groups")
 
@@ -16,53 +15,25 @@ end
 function M.load(opts)
 	opts = vim.tbl_deep_extend("force", M.options or Config.defaults, opts or {})
 
-	local is_cache = opts.cache ~= false
-	local cache_key = is_cache and Cache.generate(opts) or nil
-
-	if is_cache and cache_key and Cache.exists(cache_key) then
-		local stored = Cache.get(cache_key)
-
-		vim.g.colors_name = "sarnai-" .. opts.style
-		vim.o.background = opts.style == "ovol" and "light" or "dark"
-
-		if stored ~= nil then
-			Groups.set(stored)
-			return stored.palette
-		end
-	end
-
 	local palette = Colors.get(opts.style)
 
 	if opts.on_colors and type(opts.on_colors) == "function" then
 		opts.on_colors(palette)
 	end
 
-	if vim.g.colors_name then
-		vim.cmd("hi clear")
-	end
+	if vim.g.colors_name then vim.cmd("hi clear") end
 
 	vim.o.background = opts.style == "ovol" and "light" or "dark"
 
-	vim.o.termguicolors = true
-	if opts.terminal_colors ~= false then
+	if vim.o.termguicolors and opts.terminal_colors then
 		Term.set_colors(palette)
 	end
 
-	local hl = Groups.get(palette, opts)
-
-	Groups.set(hl)
+	Groups.setup(palette, opts)
 
 	vim.g.colors_name = "sarnai-" .. opts.style
 
-	if is_cache and cache_key then
-		Cache.store(cache_key, hl, palette)
-	end
-
 	return palette
-end
-
-function M.clear_cache()
-	Cache.clear()
 end
 
 return M
