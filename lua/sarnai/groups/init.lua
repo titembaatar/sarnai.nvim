bafb75107dd6b983b5e01070cbbe6a0c0e7887b3
titembaatar <etienne.guillemin@protonmail.com>
2026-01-28 00:13:59 +0100

    refactor: simplified

--- lua/sarnai/groups/init.lua
+++ lua/sarnai/groups/init.lua
@@ -2,60 +2,89 @@
 
 local M = {}
 
----@param palette ColorPalette
----@param config SarnaiConfig
----@return Groups
-function M.get(palette, config)
+M.groups = {
+	"base",
+	"lsp",
+	"treesitter",
+	"plugins",
+}
+
+---@param colors ColorPalette
+---@param opts SarnaiConfig
+function M.setup(colors, opts)
 	local groups = {}
 
-	groups = vim.tbl_deep_extend("force", groups, require("sarnai.groups.base").get(palette, config))
-	groups = vim.tbl_deep_extend("force", groups, require("sarnai.groups.lsp").get(palette, config))
-	groups = vim.tbl_deep_extend("force", groups, require("sarnai.groups.treesitter").get(palette, config))
-	groups = vim.tbl_deep_extend("force", groups, require("sarnai.groups.plugins").get(palette, config))
-
-	if config.on_highlights and type(config.on_highlights) == "function" then
-		config.on_highlights(groups, palette)
+	for _, group_module in ipairs(M.groups) do
+		local ok, group = pcall(require, "sarnai.groups." .. group_module)
+		if ok and type(group.get) == "function" then
+			groups = vim.tbl_extend("force", groups, group.get(colors, opts))
+		end
 	end
 
-	return groups
-end
-
----@param groups Groups
-function M.set(groups)
-	if vim.g.colors_name then
-		vim.cmd("hi clear")
+	if opts.on_highlights and type(opts.on_highlights) == "function" then
+		opts.on_highlights(groups, colors)
 	end
 
-	vim.o.termguicolors = true
+	for group, options in pairs(groups) do
+		if options.style and type(options.style) == "table" then
+			local style = options.style
 
-	for group, opts in pairs(groups) do
-		if type(opts) ~= "table" then
-			vim.notify(
-				"Invalid highlight options for group " .. group .. ": " .. tostring(opts),
-				vim.log.levels.WARN
-			)
-			goto continue
+			options.italic        = style.italic or false
+			options.bold          = style.bold or false
+			options.underline     = style.underline or false
+			options.undercurl     = style.undercurl or false
+			options.strikethrough = style.strikethrough or false
+			options.style         = nil
 		end
 
-		if opts.style and type(opts.style) == "table" then
-			local style = opts.style
-
-			opts.italic        = style.italic or false
-			opts.bold          = style.bold or false
-			opts.underline     = style.underline or false
-			opts.undercurl     = style.undercurl or false
-			opts.strikethrough = style.strikethrough or false
-			opts.style         = nil
-		end
-
-		if opts.link and type(opts.link) == "string" then
-			vim.api.nvim_set_hl(0, group, { link = opts.link })
+		if options.link and type(options.link) == "string" then
+			vim.api.nvim_set_hl(0, group, { link = options.link })
 		else
-			vim.api.nvim_set_hl(0, group, opts)
+			vim.api.nvim_set_hl(0, group, options)
 		end
-
-		::continue::
 	end
 end
+
+-- ---@param colors ColorPalette
+-- ---@param opts SarnaiConfig
+-- ---@return Groups
+-- function M.get(colors, opts)
+-- 	local groups = {}
+--
+-- 	for _, group_module in ipairs(M.groups) do
+-- 		local ok, group = pcall(require, "sarnai.groups." .. group_module)
+-- 		if ok and type(group.get) == "function" then
+-- 			groups = vim.tbl_extend("force", groups, group.get(colors, opts))
+-- 		end
+-- 	end
+--
+-- 	if opts.on_highlights and type(opts.on_highlights) == "function" then
+-- 		opts.on_highlights(groups, colors)
+-- 	end
+--
+-- 	return groups
+-- end
+--
+-- ---@param groups Groups
+-- function M.set(groups)
+-- 	for group, opts in pairs(groups) do
+-- 		if opts.style and type(opts.style) == "table" then
+-- 			local style = opts.style
+--
+-- 			opts.italic        = style.italic or false
+-- 			opts.bold          = style.bold or false
+-- 			opts.underline     = style.underline or false
+-- 			opts.undercurl     = style.undercurl or false
+-- 			opts.strikethrough = style.strikethrough or false
+-- 			opts.style         = nil
+-- 		end
+--
+-- 		if opts.link and type(opts.link) == "string" then
+-- 			vim.api.nvim_set_hl(0, group, { link = opts.link })
+-- 		else
+-- 			vim.api.nvim_set_hl(0, group, opts)
+-- 		end
+-- 	end
+-- end
 
 return M
