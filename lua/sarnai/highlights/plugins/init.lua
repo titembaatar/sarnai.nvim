titembaatar/sarnai.nvim

commit 690cc3f34828099b55a6c1649fc65fce68eb1dd0

typo

lua/sarnai/highlights/plugins/init.lua
@@ -30,7 +30,7 @@ function M.get(palette, config)
 	for lazy_name, module_name in pairs(M.plugins) do
 		if loaded[module_name] then goto continue end
 
-		if require("sarnai.util.plugins").is_enabled(lazy_name, module_name, config) then
+		if require("lua.sarnai.util.plugin").is_enabled(lazy_name, module_name, config) then
 			local success, plugin_highlights = pcall(require, "sarnai.highlights.plugins." .. module_name)
 
 			if success and type(plugin_highlights.get) == "function" then

commit 45ecf1883efa6c533fd3f636ae209ee1c5da0879

fix `require` path
Regression introduced by 690cc3f34828099b55a6c1649fc65fce68eb1dd0

lua/sarnai/highlights/plugins/init.lua
@@ -30,7 +30,7 @@ function M.get(palette, config)
 	for lazy_name, module_name in pairs(M.plugins) do
 		if loaded[module_name] then goto continue end
 
-		if require("lua.sarnai.util.plugin").is_enabled(lazy_name, module_name, config) then
+		if require("sarnai.util.plugin").is_enabled(lazy_name, module_name, config) then
 			local success, plugin_highlights = pcall(require, "sarnai.highlights.plugins." .. module_name)
 
 			if success and type(plugin_highlights.get) == "function" then