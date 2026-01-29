8f8fcfa969222001eb1c255f4cb27800a4e97db0
titembaatar <etienne.guillemin@protonmail.com>
2026-01-29 23:47:11 +0100

    fixed opts.styles options to be global

--- lua/sarnai/groups/init.lua
+++ lua/sarnai/groups/init.lua
@@ -29,11 +29,11 @@ function M.setup(colors, opts)
 		if options.style and type(options.style) == "table" then
 			local style = options.style
 
-			options.italic        = style.italic or false
-			options.bold          = style.bold or false
-			options.underline     = style.underline or false
-			options.undercurl     = style.undercurl or false
-			options.strikethrough = style.strikethrough or false
+			options.italic        = (opts.styles.italic and style.italic) or false
+			options.bold          = (opts.styles.bold and style.bold) or false
+			options.underline     = (opts.styles.underline and style.underline) or false
+			options.undercurl     = (opts.styles.undercurl and style.undercurl) or false
+			options.strikethrough = (opts.styles.strikethough and style.strikethrough) or false
 			options.style         = nil
 		end
 
@@ -44,47 +44,5 @@ function M.setup(colors, opts)
 		end
 	end
 end
-
--- ---@param colors ColorPalette
--- ---@param opts SarnaiConfig
--- ---@return Groups
--- function M.get(colors, opts)
--- 	local groups = {}
---
--- 	for _, group_module in ipairs(M.groups) do
--- 		local ok, group = pcall(require, "sarnai.groups." .. group_module)
--- 		if ok and type(group.get) == "function" then
--- 			groups = vim.tbl_extend("force", groups, group.get(colors, opts))
--- 		end
--- 	end
---
--- 	if opts.on_highlights and type(opts.on_highlights) == "function" then
--- 		opts.on_highlights(groups, colors)
--- 	end
---
--- 	return groups
--- end
---
--- ---@param groups Groups
--- function M.set(groups)
--- 	for group, opts in pairs(groups) do
--- 		if opts.style and type(opts.style) == "table" then
--- 			local style = opts.style
---
--- 			opts.italic        = style.italic or false
--- 			opts.bold          = style.bold or false
--- 			opts.underline     = style.underline or false
--- 			opts.undercurl     = style.undercurl or false
--- 			opts.strikethrough = style.strikethrough or false
--- 			opts.style         = nil
--- 		end
---
--- 		if opts.link and type(opts.link) == "string" then
--- 			vim.api.nvim_set_hl(0, group, { link = opts.link })
--- 		else
--- 			vim.api.nvim_set_hl(0, group, opts)
--- 		end
--- 	end
--- end
 
 return M
