75568cb5b10984adbe1b05acda4d8bf4c6eb85aa
titembaatar <etienne.guillemin@protonmail.com>
2026-01-30 18:28:14 +0100

    cleanup

--- lua/sarnai/init.lua
+++ lua/sarnai/init.lua
@@ -5,36 +5,24 @@ local Groups = require("sarnai.groups")
 
 local M = {}
 
----@param opts? SarnaiConfig
-function M.setup(opts)
-	M.options = vim.tbl_deep_extend("force", Config.defaults, opts or {})
-end
 
 ---@param opts? SarnaiConfig
----@return ColorPalette
 function M.load(opts)
-	opts = vim.tbl_deep_extend("force", M.options or Config.defaults, opts or {})
+	opts = require("sarnai.config").extend(opts)
 
-	local c = Colors.get(opts.style)
-
-	if opts.on_colors and type(opts.on_colors) == "function" then
-		opts.on_colors(c)
-	end
-
-	if vim.g.colors_name then vim.cmd("hi clear") end
-
-	vim.o.background = opts.style == "ovol" and "light" or "dark"
-
+	local c = Colors.get(opts)
+	local g = Groups.get(c, opts)
+	Groups.setup(g)
 	if vim.o.termguicolors and opts.terminal_colors then
 		Term.setup(c)
 	end
 
-	Groups.setup(c, opts)
-
-	vim.g.colors_name = "sarnai-" .. opts.style
-
-	return c
+  if vim.g.colors_name then vim.cmd("hi clear") end
+	vim.o.termguicolors = true
+  vim.g.colors_name = "sarnai-" .. opts.style
 end
+
+M.setup = Config.setup
 
 return M
 
